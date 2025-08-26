cd0338ad252bbab18f152f1b42f938ebb4ef48c9
Mahmoud <ma@fondative.com>
2025-08-26 12:49:51 +0200

    feat: Update AuthLayout

--- apps/web/src/app/layouts/auth-layout.tsx
+++ apps/web/src/app/layouts/auth-layout.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router';
+import Logo from '@/assets/logo.svg?react';
 
 export function AuthLayout() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <a href="#" className="flex items-center gap-2 self-center font-medium">
+        <Logo />
+      </a>
+      <div className="flex w-full max-w-sm flex-col gap-6">
         <Outlet />
       </div>
     </div>
